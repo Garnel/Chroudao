8c843b577fc1437e3d5062ee4de87facb824af28
Garnel <maogm12@gmail.com>
2013-11-28 22:15:49 +0800

    refactor

--- dict.js
+++ dict.js
@@ -2,15 +2,37 @@ var dictLoader = {
     seachYoudao: 'http://dict.youdao.com/search?q=',
     
     requestYoudao: function(key) {
-        $.get(this.seachYoudao + key, function(data) {
-            var result = $(data).find('#results-contents');
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'text/html';
+        xhr.open('GET', this.seachYoudao + key, true);
+        xhr.send(null);
+        xhr.onload = function(e) {
+            var content = document.getElementById('content');
+            content.style.visibility = 'hidden';
+            content.innerHTML = xhr.responseText;
+            var result = document.getElementById('results');
+            content.innerHTML = "";
+            content.appendChild(result);
+            content.style.visibility = 'visible';
 
-            result.find('.add_to_wordbook, .more-example, #webTrans h3, #tWebTrans,' + 
-                          '#tPETrans, #eBaike, .more, .img-list').remove();
-            result.find('#hhDictTrans, tEETrans').removeAttr('style');
-
-            $('#content').empty().append(result);
-        });
+            var tabs = document.querySelectorAll('.tabs a');
+            for (var i in tabs) {
+                var tab = tabs[i];
+                console.log(tab);
+                var cur = tab.parentNode.getElementsByClassName('tab-current');
+                tab.onclick = function(){
+                    if (cur.length > 1) {
+                        cur = cur[0];
+                        cur.className.replace('tab-current', '');
+                        var currel = cur.getAttribute('rel').slice(1);
+                        document.getElementById(currel).style.display = "none";
+                    }
+                    var rel = tab.getAttribute('rel').slice(1);
+                    tab.className += ' tab-current';
+                    document.getEle(rel).style.display = "block";
+                }
+            };
+        }
     },
 
     resultFilter: function(result) {
@@ -19,18 +41,20 @@ var dictLoader = {
 };
 
 var search = function() {
-    var key = $("#keyword").val();
-    dictLoader.requestYoudao(key);
+    var key = document.getElementById("keyword").value;
+    if (key !== "") {
+        dictLoader.requestYoudao(key);
+    }
 };
 
-$(document).ready(function(){
-    $('#search').click(function(){
+onload = function(){
+    document.getElementById('search').onclick = function(){
         search();
-    });
+    };
+};
 
-    $('#keyword').keypress(function(e) {
-        if (e.which == 13) {
-            search();
-        }
-    });
-});
+document.onkeypress  = function(e) {
+    if (e.which == 13) {
+        search();
+    }
+}
